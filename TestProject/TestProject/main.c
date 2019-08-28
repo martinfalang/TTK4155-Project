7f2b128c5f85f9507bda349165138c9366a635c1
Eirik Falck <eirik.falck@gmail.com>
2019-08-28 12:46:16 +0200

    added testproject to blink LED on pin PA0

---
+++ TestProject/TestProject/main.c
@@ -0,0 +1,24 @@
+/*
+ * TestProject.c
+ *
+ * Created: 28.08.2019 12.19.50
+ * Author : eirik
+ */ 
+
+#define F_CPU 4915200
+
+#include <avr/io.h>
+#inclue <util/delay.h>
+
+
+int main(void)
+{
+    DDRA |= 1 << DDA0;
+	
+    while (1) 
+    {
+		_delay_ms(500);
+		PORTA ^= 1 << PA0;
+    }
+}
+
