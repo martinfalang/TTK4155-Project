687e8c2bf597f4a956f78426503597fb08ad1442
jaf111 <jan_aleksander@hotmail.com>
2019-08-29 15:08:59 +0200

    Working uart implementation

--- src/main.c
+++ src/main.c
@@ -3,22 +3,25 @@
  *
  * Created: 28.08.2019 12.19.50
  * Author : eirik
- */ 
+ */
 
 #define F_CPU 4915200
 
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "uart-driver.h"
 
 int main(void)
 {
-    DDRA |= 1 << DDA0;
-	
-    while (1) 
-    {
-		_delay_ms(500);
-		PORTA ^= 1 << PA0;
-    }
-}
+  uart_init();
 
+  DDRA |= 1 << DDA0;
+
+  while (1)
+  {
+    uart_transmit('o');
+    _delay_ms(500);
+    PORTA ^= 1 << PA0;
+  }
+}
--- src/uart-driver.c
+++ src/uart-driver.c
@@ -1,39 +1,42 @@
 #ifndef UART_DRIVER
 #define UART_DRIVER
 
+#include <avr/io.h>
+#include "uart-driver.h"
+
 // TODO: Include header file defining macros?
 
-#define FOSC 1843200 // Clock Speed
+#define FOSC 4915200 // Clock Speed
 #define BAUD 9600
-#define MYUBRR FOSC / 16 / BAUD - 1 // Baud rate register
+#define MYUBRR (FOSC / 16 / BAUD - 1) // Baud rate register
 
 void uart_init() // ubrr = uart baud rate register
 {
     /** Initialize the uart unit */
     /* Set baud rate */
-    UBRRH = (unsigned char)(MYUBRR >> 8); // high byte
-    UBRRL = (unsigned char)(MYUBRR);      // low byte
+    UBRR0H = (unsigned char)(MYUBRR >> 8); // high byte
+    UBRR0L = (unsigned char)(MYUBRR);      // low byte
     /* Enable receiver and transmitter */
-    UCSRB = (1 << RXEN) | (1 << TXEN);
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
     /* Set frame format: 8data, 2stop bit */
-    UCSRC = (1 << URSEL) | (1 << USBS) | (3 << UCSZ0);
+    UCSR0C = (1 << URSEL0) | (1 << USBS0) | (3 << UCSZ00);
 }
 
 void uart_transmit(char data)
 {
     /** Transmit one char of data to the mcu */
-    while (!(UCSRA & (1 << UDRE)))
+    while (!(UCSR0A & (1 << UDRE0)))
         ; // Wait for transmit buffer to be empty
 
-    UDR = data; // Put data in UDR - usart data register
+    UDR0 = data; // Put data in UDR - usart data register
 }
 
 char uart_recieve()
 {
     /** Recieve one char of data from the mcu */
-    while (!(UCSRA & (1 << RXC)))
+    while (!(UCSR0A & (1 << RXC0)))
         ; // Wait for data recieve
     
-    return UDR; // Return the data in the data register
+    return UDR0; // Return the data in the data register
 }
 #endif /* UART_DRIVER */
