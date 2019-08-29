fd73f6794d1a19be10720dfb29db6f6966832365
Martin Falang <falang.martin@gmail.com>
2019-08-29 16:03:42 +0200

    Implemented printf. Gives compiler warnings, but works

--- src/main.c
+++ src/main.c
@@ -7,21 +7,26 @@
 
 #define F_CPU 4915200
 
+#include <stdio.h>
+
 #include <avr/io.h>
 #include <util/delay.h>
 
 #include "uart.h"
 
-void heart_beat() {
-    PORTA ^= 1 << PA0;
+void heart_beat()
+{
+  PORTA ^= 1 << PA0;
 }
 
-void test_uart() {
-     uart_transmit('p');
+void test_uart()
+{
+  uart_transmit('p');
 }
 
 int main(void)
 {
+  fdevopen(uart_transmit, uart_receive); // Ask about compiler warnings
   uart_init();
 
   DDRA |= 1 << DDA0;
@@ -31,12 +36,11 @@ int main(void)
     //heart_beat();
     //test_uart();
 
-    if (uart_recieve()) {
-      heart_beat();
-    }
+    // if (uart_recieve()) {
+    //   heart_beat();
+    // }
+    printf("Hei på deg\n");
 
-    // _delay_ms(500);
-    
-
+    _delay_ms(500);
   }
 }
