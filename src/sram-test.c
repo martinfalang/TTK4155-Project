d7dd0bccb6ed86fea1131068aa6f9aff53e23aa7
noragr <noragr@stud.ntnu.no>
2019-09-03 19:59:50 +0200

    masked out jtag pins, made sram test systematically write increasing number

--- src/sram-test.c
+++ src/sram-test.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
+
 void SRAM_test(void)
 {
     volatile char *ext_ram = (char *)0x1800; // Start address for the SRAM
@@ -16,13 +17,16 @@ void SRAM_test(void)
     // Write phase: Immediately check that the correct value was stored
     srand(seed);
     printf("Starting write phase...\n");
+    uint8_t d = 0;
     for (uint16_t i = 0; i < ext_ram_size; i++)
     {
-        uint8_t some_value = rand();
+        uint8_t some_value = d; //rand();
+        ++d;
         ext_ram[i] = some_value;
         uint8_t retreived_value = ext_ram[i];
         if (retreived_value != some_value)
         {
+
             printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i,
                    retreived_value, some_value);
             write_errors++;
@@ -34,9 +38,11 @@ void SRAM_test(void)
 
     // Retrieval phase: Check that no values were changed during or after the write phase
     srand(seed); // reset the PRNG to the state it had before the write phase
+    d = 0;
     for (uint16_t i = 0; i < ext_ram_size; i++)
     {
-        uint8_t some_value = rand();
+        uint8_t some_value = d;  // rand();
+        ++d;
         uint8_t retreived_value = ext_ram[i];
         if (retreived_value != some_value)
         {
--- src/xmem.c
+++ src/xmem.c
@@ -4,5 +4,5 @@
 void xmem_init(void) {
     // Enable sram in control register
     MCUCR |= (1 << SRE); // changes to alternate function for external memory pins
-    SFIOR |= (1 << XMM0);  // masks out upper 4 bits of adress bus used by jtag
+    SFIOR |= (1 << XMM2);  // masks out upper 4 bits of adress bus used by jtag
 }
