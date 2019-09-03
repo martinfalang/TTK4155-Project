cb9fa57c2d2c1ec8ef1f48fcaf9a90ac13104fc5
noragr <noragr@stud.ntnu.no>
2019-09-03 17:27:32 +0200

    added sudo and she-bang to serial script

--- open-serial.sh
+++ open-serial.sh
@@ -1,7 +1,9 @@
-# Must be run with sudo
+#! /bin/sh
+
 # Opens byggern serial port with baudrate 9600, 2 stopbits
 # and corrent line endings.
 # 
 # All flags with description can be found here: https://github.com/npat-efault/picocom/blob/master/picocom.1.md
 
-picocom -b 9600 -p 2 --imap lfcrlf /dev/ttyS0
+echo "Running with sudo, enter password"
+sudo picocom -b 9600 -p 2 --imap lfcrlf /dev/ttyS0
