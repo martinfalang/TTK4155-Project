456c536efe5ad431b1309f9366dd0057df9c8085
noragr <noragr@stud.ntnu.no>
2019-09-11 08:51:46 +0200

    Remembered to save after fixing merge conflict

--- src/main.c
+++ src/main.c
@@ -19,25 +19,16 @@
 
 void heart_beat()
 {
-<<<<<<< HEAD
   PORTB ^= 1 << PB0;
-=======
-  PORTA ^= 1 << PA0;
->>>>>>> origin
 }
 
 void test_uart()
 {
-<<<<<<< HEAD
   printf("Hei\n");
-=======
-  uart_transmit('p');
->>>>>>> origin
 }
 
 int main(void)
 {
-<<<<<<< HEAD
   DDRB |= 1 << DDB0;
   
   uart_init(); // So we can communicate with the terminal connected via JTAG
@@ -47,23 +38,5 @@ int main(void)
     heart_beat();
     test_uart();
     _delay_ms(100);
-=======
-  fdevopen(uart_transmit, uart_receive); // Ask about compiler warnings
-  uart_init();
-
-  DDRA |= 1 << DDA0;
-
-  while (1)
-  {
-    //heart_beat();
-    //test_uart();
-
-    // if (uart_recieve()) {
-    //   heart_beat();
-    // }
-    printf("Hei på deg\n");
-
-    _delay_ms(500);
->>>>>>> origin
   }
 }
