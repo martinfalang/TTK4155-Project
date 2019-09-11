cea20f4288ed195c31e865242dad4c4d1cbbd46c
Martin Falang <falang.martin@gmail.com>
2019-09-11 17:37:53 +0200

    reverted to adc polling because it was easier

--- src/adc.c
+++ src/adc.c
@@ -1,34 +1,21 @@
-#include <avr/interrupt.h>
 #include "adc.h"
 
 
 volatile uint8_t *adc = (uint8_t *) ADC_BASE;
-volatile uint8_t adc_data = 0;
 
 void adc_init() {
     // Setting Pin D2 (INTR) to input
     DDRD &= ~(1 << DDD2);
     // Enable pull up resistor
     PORTD |= (1 << PD2);
-    
-    // Trigger on falling edge
-    MCUCR |= (1 << ISC01); 
-    MCUCR &= ~(1 << ISC00);
-
-    // Enable interrupt 0
-    GICR |= (1 << INT0);
 }
 
 
-void adc_start_conversion(adc_channel_t channel) {
+uint8_t adc_read(adc_channel_t channel) {
+    // decided against using interrupts because polling gave
+    // a cleaner interface. 
+
     *adc = channel;
-}
-
-
-uint8_t adc_read() {
-    return adc_data;
-}
-
-ISR(INT0_vect) {
-    adc_data = *adc;
+    while (PIND & (1<<PD2));  // wait for conversion to finish
+    return *adc;
 }
