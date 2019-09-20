bc744a0d70c588b390d6e429625b80a370fb604f
Eirik Falck <eirik.falck@gmail.com>
2019-09-20 09:19:43 +0200

    changed delay to single .h file with implementation instead of .h and .c

--- src/delay.c
+++
@@ -1,8 +0,0 @@
-#include "delay.h"
-
-
-void var_delay_ms(int ms) {
-    while (ms--) {
-        _delay_ms(1);
-    }
-}
--- src/delay.h
+++ src/delay.h
@@ -3,7 +3,11 @@
 
 #include <util/delay.h>
 
-void var_delay_ms(int ms);
+void var_delay_ms(int ms) {
+    while (ms--) {
+        _delay_ms(1);
+    }
+}
 
 
 #endif // DELAY_H
