845083eabfeb15e45d855d556faf04e43871195b
Martin Falang <falang.martin@gmail.com>
2019-10-23 17:41:31 +0200

    Finish can. Cleanup files

--- Node2/src/main.c
+++ Node2/src/main.c
@@ -1,28 +1,33 @@
 
 #include <stdio.h>
 #include <avr/io.h>
-#include "uart.h"
+#include "../../lib/inc/uart.h"
 
-#include "defines.h"
+#include "../../lib/inc/defines.h"
 #include <util/delay.h>
 
-#include "spi.h"
-#include "can.h"
+#include "../../lib/inc/spi.h"
+#include "../../lib/inc/can.h"
+#include "../../lib/inc/mcp2515_defines.h"
 
 
 int main(void) {
     uart_init();
 
     //spi_init();
-    //can_init(MODE_LOOPBACK);
+    can_init(MODE_NORMAL);
 
     printf("All inits ran successfully!\n");
 
     while (1) {
-	    printf("Hello world!\n");
-        
-        can_test();
+        const can_msg_t *recv_msg;
 
-        _delay_ms(1000);
+        if (can_new_msg()) {
+            recv_msg = can_get_recv_msg();
+            printf("\n\nRecv:\n");
+            can_print_msg(recv_msg);
+            can_send(recv_msg);
+        }
+        // _delay_ms(1);
     }
 }
