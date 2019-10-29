a36b7e25623d8ac20ff832ec8fc7f8bc22871745
Eirik Falck <eirik.falck@gmail.com>
2019-10-29 08:10:26 +0100

    add documentation to functions; add x and y direction to game object struct

--- pong/pong.h
+++ pong/pong.h
@@ -15,15 +15,29 @@ typedef struct pong_object {
     int y;  // y pos
     int w;  // width
     int h;  // height
-    int dir;  // direction of travel
+    int dirx;  // direction of travel in x direction
+    int diry;  // direction of travel in y direction
 } pong_object_t;
 
 typedef pong_object_t pong_ball_t;
 typedef pong_object_t pong_paddle_t;
 
-
+/**
+ * @brief Check collision (i.e. overlap) between two pong objects
+ * 
+ * @param obj1 Object 1
+ * @param obj2 Object 2
+ * @return true If they overlap/have collidied
+ * @return false If they don't overlap/haven't collided
+ */
 bool pong_check_collision(pong_object_t obj1, pong_object_t obj2);
 
+/**
+ * @brief Update all game objects' position
+ * 
+ * @param objarr 
+ */
+void pong_update_objects_position(pong_object_t[] objarr);
 
 
 #endif  // PONG_H
