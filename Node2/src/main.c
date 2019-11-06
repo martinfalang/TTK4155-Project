214f57173d7b7840161831b5f43cb829337646d4
noragr <noragr@stud.ntnu.no>
2019-11-06 16:58:40 +0100

    actually fix merge conflict

--- Node2/src/main.c
+++ Node2/src/main.c
@@ -18,27 +18,17 @@
 
 int main(void) {
     uart_init();
-<<<<<<< HEAD
     motor_init();
     encoder_init();    
     solenoid_init();
     can_init(MODE_NORMAL);
-=======
-    
-    pid_t motor_pid;
-    pid_init(&motor_pid, 0, 0, 0, 250);
-
->>>>>>> node2-pi-controller
     printf("All inits ran successfully!\n");
 
     _delay_ms(100);
     solenoid_give_pulse();
 
     while (1) {
-<<<<<<< HEAD
         
         _delay_ms(10);
-=======
->>>>>>> node2-pi-controller
     }
 }
