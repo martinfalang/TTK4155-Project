c470397f28095b318d9f0a330e3ccf2bb7c3ecbb
Martin Falang <falang.martin@gmail.com>
2019-11-11 09:19:41 +0100

    PID works ish. Motor makes unwanted noise

--- Node2/src/main.c
+++ Node2/src/main.c
@@ -19,11 +19,12 @@
 
 
 pid_t motor_pid;
-float kp = 1.1;
+float kp = 0.8;
 float ki = 0.01;
-float kd = 0.05;
+float kd = 0.08;
 float t  = 0.01;  // sample time of pid
 
+int16_t pos;
 
 int main(void) {
     uart_init();
@@ -46,8 +47,8 @@ int main(void) {
 
             int16_t r = recvmsg->data[1] - 51;
             motor_pid.setpoint = r;
-            printf("Enc: %i\n", encoder_read());
-            printf("r: %d   u: %d\n", (int)(10 * motor_pid.setpoint), (int)(10 * motor_pid.output));
+            printf("Pos: %i\n", pos);
+            // printf("r: %d   u: %d\n", (int)(10 * motor_pid.setpoint), (int)(10 * motor_pid.output));
         }
     }
 
@@ -56,6 +57,8 @@ int main(void) {
 
 
 ISR(TIMER5_COMPA_vect) {
+    // int16_t enc = encoder_read();
+    // pos += enc;
     pid_next_output(&motor_pid);
     motor_pid.measurement = encoder_read();
     motor_set_speed(motor_pid.output);
