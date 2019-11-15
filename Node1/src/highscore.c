223ac120013f8a0fdaae8f92b97877c00c989772
= <krisbrud@stud.ntnu.no>
2019-11-15 16:08:49 +0100

    High score first draft implemented. Not tested yet.

---
+++ Node1/src/highscore.c
@@ -0,0 +1,73 @@
+#include "highscore.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+#include <string.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+////////////////////////////////////////////////////////////////////////////////
+#define NUM_HIGHSCORES 3
+
+////////////////////////////////////////////////////////////////////////////////
+// Global variables
+////////////////////////////////////////////////////////////////////////////////
+static uint16_t _highscores[NUM_HIGHSCORES];
+
+////////////////////////////////////////////////////////////////////////////////
+// Private function declarations
+////////////////////////////////////////////////////////////////////////////////
+void _insert_score(uint8_t place, uint16_t score);
+
+////////////////////////////////////////////////////////////////////////////////
+// Public functions
+////////////////////////////////////////////////////////////////////////////////
+void highscore_init(void) {
+    _highscores[0] = 15;
+    _highscores[1] = 10;
+    _highscores[2] = 5;
+}
+
+uint16_t highscore_get(uint8_t place) {
+    if (1 <= place && place <= NUM_HIGHSCORES) {
+        return _highscores[place];
+    }
+    return -1;
+};
+
+void highscore_print_score(char * out, uint8_t place) {
+    uint16_t score = highscore_get(place);
+
+    sprintf(out, "#%d: %d", place, score);
+};
+
+uint8_t highscore_nominate(uint16_t score) {
+    // Assumes the highscore list is already sorted 
+    for (uint8_t p = 1; p <= NUM_HIGHSCORES; ++p) { // p for place
+        if (score > highscore_get(p)) {
+            _insert_score(p + 1, score);
+            return highscore_get(p);
+        }
+    }
+    return 0; // Score not good enough
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Private functions
+////////////////////////////////////////////////////////////////////////////////
+void _insert_score(uint8_t place, uint16_t score) {
+    // Inserts the score at the given position, discarding the last element
+    // Note that place is not zero-indexed
+
+    // EXAMPLE:
+    // _highscores before:
+    // [15, 10, 5]
+    // _highscores after _insert_score(13)
+    // [15, 13, 10]
+
+    for (uint8_t i = NUM_HIGHSCORES - 1; i > place - 1; --i) {
+        _highscores[i] = _highscores[i-1]
+    }
+    _highscores[place - 1] = score;
+}
---
+++ Node1/src/highscore.h
@@ -0,0 +1,43 @@
+#ifndef HIGHSCORE_H
+#define HIGHSCORE_H
+
+#include <stdint.h>
+
+#define HIGHSCORE_STR_MAX_LENGTH 19
+
+/**
+ * @brief Initializes the highscore list to a predefined state
+ * 
+ */
+void highscore_init(void);
+
+/**
+ * @brief Gets the score associated with the given place, and -1 if it doesn't
+ *        exist.
+ *        E.g. highscore_get(1) returns the first place, 
+ *        highscore_get(3) the third place etc.
+ * 
+ * @param place 
+ * @return uint16_t 
+ */
+uint16_t highscore_get(uint8_t place);
+
+/**
+ * @brief Writes the score of given place to the string out. Guaranteed to not be
+ *        longer than HIGHSCORE_STR_MAX_LENGTH
+ * 
+ * @param out 
+ * @param max_length 
+ */
+void highscore_print_score(char * out, uint8_t place);
+
+/**
+ * @brief Places the high score in the list if it is good enough.
+ * 
+ * @param score The candidate score
+ * @return uint8_t The placement in the high score list if good enough. 
+ *                 0 if it is not good enough
+ */
+uint8_t highscore_nominate(uint16_t score);
+
+#endif  /* HIGHSCORE_H */
