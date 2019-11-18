aabce2cbdc3ab0facc0d3973cc7c153d8cf0c5b8
Eirik Falck <eirik.falck@gmail.com>
2019-11-18 19:05:49 +0100

    add doxygen docs to servo file

--- Node2/src/servo.h
+++ Node2/src/servo.h
@@ -1,3 +1,16 @@
+/**
+ * @file servo.h
+ * @author Kristian Brudeli
+ *         Martin Falang
+ *         Eirik Flemsæter Falck
+ * @brief Implements functions for controller the servo motor. 
+ * @version 0.1
+ * @date 2019-11-18
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #ifndef SERVO_H
 #define SERVO_H
 
